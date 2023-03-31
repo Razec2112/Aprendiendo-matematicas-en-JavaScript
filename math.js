ad96d98eef3723de2597f6b6aecff5238be7df53
Razec2112 <razec.cz@gmail.com>
2023-03-31 15:34:45 -0400

    se agrega el calculo de una circunferencia con la ayuda de Math

--- math.js
+++ math.js
@@ -43,3 +43,30 @@ console.log({
     areaTriangulo,
 })
 console.groupEnd("Triangulo")
+
+console.group("Circulo")
+
+const radio = 5
+const diametroCirculo = radio * 2
+const pi = 3.1416
+const perimetroCirculo = diametroCirculo * pi
+const arearCirculo = (diametroCirculo ** 2) //asi tambien se puede expresar una potencia
+
+function calcularCirculo (radio) {
+    const diametro = radio * 2
+    return {
+        diametro: diametro,
+        perimetro: diametro * Math.PI,
+        area: (Math.pow(diametro, 2)) * Math.PI // el Math.pow me permite elevar el primer dato a la potencia del segundo dato y el Math.PI me arroja el numero PI de manera mas automatica y con mas decimales
+    }
+}
+
+console.log ({
+radio,
+diametroCirculo,
+pi,
+perimetroCirculo,
+arearCirculo,
+    
+})
+console.groupEnd("Circulo")
